92eeaf3b0d35b718a666b39d3be0d513301822ca
sigthorz <54600572+sigthorz@users.noreply.github.com>
2021-03-28 20:54:02 +0000

    Add files via upload

---
+++ verk3/liga.js
@@ -0,0 +1,68 @@
+(function () {
+    'use strict';
+    function supportsProperty(p) {
+        var prefixes = ['Webkit', 'Moz', 'O', 'ms'],
+            i,
+            div = document.createElement('div'),
+            ret = p in div.style;
+        if (!ret) {
+            p = p.charAt(0).toUpperCase() + p.substr(1);
+            for (i = 0; i < prefixes.length; i += 1) {
+                ret = prefixes[i] + p in div.style;
+                if (ret) {
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+    var icons;
+    if (!supportsProperty('fontFeatureSettings')) {
+        icons = {
+            'spotify': '&#xea94;',
+            'brand14': '&#xea94;',
+            'twitter': '&#xea96;',
+            'brand16': '&#xea96;',
+            'youtube': '&#xea9d;',
+            'brand21': '&#xea9d;',
+            'steam2': '&#xeaad;',
+            'brand37': '&#xeaad;',
+            'github': '&#xeab0;',
+            'brand40': '&#xeab0;',
+            'paypal': '&#xead8;',
+            'brand79': '&#xead8;',
+          '0': 0
+        };
+        delete icons['0'];
+        window.icomoonLiga = function (els) {
+            var classes,
+                el,
+                i,
+                innerHTML,
+                key;
+            els = els || document.getElementsByTagName('*');
+            if (!els.length) {
+                els = [els];
+            }
+            for (i = 0; ; i += 1) {
+                el = els[i];
+                if (!el) {
+                    break;
+                }
+                classes = el.className;
+                if (/icon-/.test(classes)) {
+                    innerHTML = el.innerHTML;
+                    if (innerHTML && innerHTML.length > 1) {
+                        for (key in icons) {
+                            if (icons.hasOwnProperty(key)) {
+                                innerHTML = innerHTML.replace(new RegExp(key, 'g'), icons[key]);
+                            }
+                        }
+                        el.innerHTML = innerHTML;
+                    }
+                }
+            }
+        };
+        window.icomoonLiga();
+    }
+}());
